c2ba928e0e0903599788fcb55df83ed98497d7a7
Marcos Navarro <marcosnpcogollos@gmail.com>
2015-06-28 15:56:00 +0200

    Sequelite y SQLite3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,19 @@
+var models = require('../models/models.js');
 
 // GET /quizes/question
 exports.question = function(req, res){
-    res.render('quizes/question', {title: 'Quiz', pregunta: 'Capital de Italia'});
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizes/question', { title: 'Quiz', pregunta: quiz[0].pregunta });
+  });
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-    if(req.query.respuesta.toLowerCase() === "roma"){
-        res.render('quizes/answer', {title: 'Quiz', respuesta: 'Correcto'});
+  models.Quiz.findAll().success(function(quiz){
+    if(req.query.respuesta.toLowerCase() === quiz[0].respuesta){
+        res.render('quizes/answer', {title: 'Quiz', cl: 'alert-success', respuesta: 'Correcto'});
     }else{
-        res.render('quizes/answer', {title: 'Quiz', respuesta: 'Incorrecto'});
+        res.render('quizes/answer', {title: 'Quiz', cl: 'alert-danger', respuesta: 'Incorrecto'});
     }
+  });
 };
---
+++ models/models.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+              {dialect: 'sqlite', storage: 'quiz.sqlite'});
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
+// sequelize.sync() crea e inicializa la tabla de preguntas de la BBDD
+sequelize.sync().success(function(){
+  // success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function (count){
+    if(count === 0){
+      // la tabla se inicializa solo si esta está vacía
+      Quiz.create({
+        pregunta: '¿Capital de Italia?',
+        respuesta: 'roma'
+      })
+      .success(function(){ console.log('Base de datos inicializada'); });
+    }
+  });
+});
