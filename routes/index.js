339d060dd3d9be2a0674d42bcd5bdc2e30da85cb
Marcos Navarro <marcosnpcogollos@gmail.com>
2015-07-18 16:49:35 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new); // formulario login
@@ -30,8 +31,10 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired,  quizC
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', authoController.creditos);
 
